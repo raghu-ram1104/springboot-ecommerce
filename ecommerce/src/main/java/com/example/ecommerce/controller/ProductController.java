0563e6144c117af7bff5090455213af19e803597
Raghuram Srikanth <141855144+rs0657@users.noreply.github.com>
2026-03-16 11:15:41 +0530

    Added product controller and fixed HTML form

--- ecommerce/src/main/java/com/example/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -4,10 +4,12 @@ import com.example.ecommerce.service.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.ecommerce.entity.*;
+import java.util.*;
 
 @Controller
 
@@ -24,5 +26,12 @@ public class ProductController {
 	public String get2(Product p) {
 		s.set1(p);
 		return "sucess";
+	}
+	
+	@GetMapping("/products")
+	public String get3(Model m) {
+		List<Product> l=s.set2();
+		m.addAttribute("products", l);
+		return "productlist";
 	}
 }
--- ecommerce/src/main/java/com/example/ecommerce/service/ProductService.java
+++ ecommerce/src/main/java/com/example/ecommerce/service/ProductService.java
@@ -1,11 +1,10 @@
 package com.example.ecommerce.service;
-
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.ecommerce.entity.*;
 import com.example.ecommerce.dao.*;
-
-
+;
 @Service
 public class ProductService {
 	@Autowired
@@ -14,5 +13,10 @@ public class ProductService {
 	public void set1(Product p)
 	{
 		r.save(p);
+	}
+	
+	public List<Product> set2()
+	{
+		return r.findAll();
 	}
 }
